05242eed8f4a69c2178a77d852bc74d2615c76cd
ansuyachauhan <28599718+ansuyachauhan@users.noreply.github.com>
2024-04-03 20:16:42 -0500

    Initial Commit

---
+++ CardsApp/CardsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsAppApp.swift
+//  CardsApp
+//
+//  Created by Ansuya Chauhan on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CardsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
